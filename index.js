77902ad407595ae57babe85be664a41160e7f4c4
Esther Kang <eskang87@gmail.com>
2021-12-16 21:40:31 -0800

    commit tree d3cd9c9267993113e6e9f1d28937948991955e90

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Add your code here
+function submitData(userName, userEmail){
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      //"Content-Type": "application/json",
+      //Accept: "application/json",
+    },
+    body: JSON.stringify({name: `${userName}`, email: `${userEmail}`})
+  })
+  .then(response => response.json())
+  .then(data => {
+    //console.log(data);
+    //console.log(data.id);
+    const body = document.querySelector('body');
+    //const div = document.createElement('div');
+    const newItemId = data.id
+    const p = document.createElement('p');
+    p.textContent = newItemId;
+    body.append(p);
+    //div.appendChild(p);
+    //body.appendChild(div);
+    //console.log(body);
+  })
+  .catch(errorObj => {
+    //alert('SOMETHING WENT WRONG!')
+    //alert(`${errorObj.message}`)
+   //console.log(errorObj.message)
+   const body = document.querySelector('body');
+   const errorMessage = errorObj.message
+   const p = document.createElement('p');
+    p.textContent = errorMessage;
+    body.append(p);
+  })
+}
+
+
+
+
+
+
+
+/*
+const formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  });
+*/
